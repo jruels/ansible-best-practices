bc8190e0354a382de1b6872b6e60b7204a4b151a
Jason Smith <jason@smithss.org>
2022-09-10 20:17:08 -0700

    inventory lab

---
+++ labs/inventory/scripts/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Need to take a tag variable
+# mediavars to test media
+# webservervars to test webservers
+# hostvar to test host var
+
+case "$1" in
+  mediavars)
+    ansible-playbook -i /home/ansible/inventory /home/ansible/scripts/backup.yml --tags "mediavars"
+    exit $?
+    ;;
+  webservervars)
+    ansible-playbook -i /home/ansible/inventory /home/ansible/scripts/backup.yml --tags "webservervars"
+    exit $?
+    ;;
+  hostvar)
+    ansible-playbook -i /home/ansible/inventory /home/ansible/scripts/backup.yml --tags "hostvar"
+    exit $?
+    ;;
+  *)
+    ansible-playbook -i /home/ansible/inventory /home/ansible/scripts/backup.yml
+    exit $?
+esac
